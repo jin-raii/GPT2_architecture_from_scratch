84cf6475215dfb416c64f62c324fbdd910467d75
Rajesh Rai <jinrai577@gmail.com>
2025-12-12 19:52:08 +0545

    update and load dataset with python-codes-25k

--- data/raw/parse_python.py
+++ data/raw/parse_python.py
@@ -10,16 +10,13 @@ Usage:
     parse_python.py <file>
 
 """)
-    exit(1)
+    sys.exit(1)
 
 def read_file_to_string(filename):
-    f = open(filename, 'rt')
-    s = f.read()
-    f.close()
-    return s
+    with open(filename, 'rt') as f:
+        return f.read()
 
 def parse_file(filename):
-    global c, d
     tree = ast.parse(read_file_to_string(filename), filename)
     
     json_tree = []
@@ -39,7 +36,7 @@ def parse_file(filename):
         children = []
         for item in l:
             children.append(traverse(item))
-        if (len(children) != 0):
+        if children:
             json_node['children'] = children
         return pos
         
@@ -52,25 +49,25 @@ def parse_file(filename):
         if isinstance(node, ast.Name):
             json_node['value'] = node.id
         elif isinstance(node, ast.Num):
-            json_node['value'] = unicode(node.n)
+            json_node['value'] = str(node.n)
         elif isinstance(node, ast.Str):
-            json_node['value'] = node.s.decode('utf-8')
+            json_node['value'] = node.s
         elif isinstance(node, ast.alias):
-            json_node['value'] = unicode(node.name)
+            json_node['value'] = node.name
             if node.asname:
                 children.append(gen_identifier(node.asname))
         elif isinstance(node, ast.FunctionDef):
-            json_node['value'] = unicode(node.name)
+            json_node['value'] = node.name
         elif isinstance(node, ast.ClassDef):
-            json_node['value'] = unicode(node.name)
+            json_node['value'] = node.name
         elif isinstance(node, ast.ImportFrom):
             if node.module:
-                json_node['value'] = unicode(node.module)
+                json_node['value'] = node.module
         elif isinstance(node, ast.Global):
             for n in node.names:
                 children.append(gen_identifier(n))
         elif isinstance(node, ast.keyword):
-            json_node['value'] = unicode(node.arg)
+            json_node['value'] = node.arg
         
 
         # Process children.
@@ -122,16 +119,16 @@ def parse_file(filename):
         else:
             # Default handling: iterate over children.
             for child in ast.iter_child_nodes(node):
-                if isinstance(child, ast.expr_context) or isinstance(child, ast.operator) or isinstance(child, ast.boolop) or isinstance(child, ast.unaryop) or isinstance(child, ast.cmpop):
+                if isinstance(child, (ast.expr_context, ast.operator, ast.boolop, ast.unaryop, ast.cmpop)):
                     # Directly include expr_context, and operators into the type instead of creating a child.
-                    json_node['type'] = json_node['type'] + type(child).__name__
+                    json_node['type'] += type(child).__name__
                 else:
                     children.append(traverse(child))
                 
         if isinstance(node, ast.Attribute):
             children.append(gen_identifier(node.attr, 'attr'))
                 
-        if (len(children) != 0):
+        if children:
             json_node['children'] = children
         return pos
     
@@ -142,6 +139,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         PrintUsage()
     try:
-        print (parse_file(sys.argv[1]))
+        print(parse_file(sys.argv[1]))
     except (UnicodeEncodeError, UnicodeDecodeError):
         pass
