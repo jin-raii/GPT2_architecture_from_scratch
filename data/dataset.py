e73d7f03b063f3558d6f29c850476bcae0dbc626
Rajesh Rai <jinrai577@gmail.com>
2025-12-13 19:13:50 +0545

    collate_fun in dataloader to add padded tokens to make sure each tokens has same sequence length

--- data/dataset.py
+++ data/dataset.py
@@ -1,5 +1,5 @@
 from torch.utils.data import Dataset
-
+import torch
 
 
 
@@ -34,3 +34,27 @@ class InstructDataset(Dataset):
     
     def __getitem__(self, index):
         return self.encoded_texts[index]
+    
+
+def collate_fn(batch, pad_token=50256, device='cpu'):
+    # find the longest sequence on the batch 
+    batch_max_length = max(len(item) + 1 for item in batch)
+
+    #pad and prepare input 
+    inputs_list, targets_lst = []
+
+    for item in batch: 
+        new_item = item.copy()
+        new_item += [pad_token]
+
+        padded = (
+            new_item + [pad_token] * (batch_max_length - len(new_item))
+        )
+
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+        inputs_list.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_list).to(device)
+    return inputs_tensor, targets
