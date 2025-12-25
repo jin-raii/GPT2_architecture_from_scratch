479c5a4dfa678150106251da57bc687ab802bab2
Rajesh Rai <jinrai577@gmail.com>
2025-12-25 16:44:48 +0545

    LayerNormalization class

--- layer_norm/layer_norm.py
+++ layer_norm/layer_norm.py
@@ -1,8 +1,18 @@
+import torch 
 import torch.nn as nn 
 
 class LayerNorm(nn.Module): 
-    def __init__(self): 
+    def __init__(self, emb_dim): 
         super().__init__()
+        self.eps = 1e-5 
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
 
     def forward(self, x): 
-        pass 
+        mean = x.mean(dim=-1, keepdim=True)
+        variance = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(variance + self.eps)
+
+        return self.scale * norm_x + self.shift
+    
+    
